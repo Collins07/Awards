104857fd532e1497fe45c562577a33065c7b3210
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-13 01:03:25 +0300

    chnages on css and templates

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
@@ -11,5 +14,9 @@ urlpatterns = [
     path('search/', views.search_project, name='search'),
     path('profile/<username>/update', views.edit_profile, name='update'),
     path('<username>/profile', views.user_profile, name='userprofile'),
+    path('project/<id>', views.project, name='project'),
 
 ] 
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
