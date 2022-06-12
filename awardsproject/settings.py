540ff3bc63ad314ddf3f1f36a065ace8888e2827
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-12 04:43:39 +0300

    set up cloudinary

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+cloudinary.config( 
+  cloud_name = config("CLOUDINARY_NAME"), 
+  api_key = config("CLOUDINARY_KEY"), 
+  api_secret = config("CLOUDINARY_SECRET") 
+) 
