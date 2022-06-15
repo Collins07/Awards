2b6652833cfba63292851e64307bb5e433de76d6
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-15 10:09:08 +0300

    ratings functionality

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django_registration.backends.one_step.views import RegistrationView
 from django.contrib.auth import views 
+from django.conf import settings
+from django.conf.urls.static import static 
 
 
 urlpatterns = [
@@ -29,3 +31,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('',include('awards.urls')),
 ]
+
+urlpatterns += static (settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += static (settings.STATIC_URL, document_root= settings.STATIC_ROOT)
